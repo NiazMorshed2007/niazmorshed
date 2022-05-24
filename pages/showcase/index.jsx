55bf32f1b3688fae8e520f9072115b80bba9bfea
NiazMorshed2007 <niazmorshed2007@gmail.com>
2022-05-24 22:21:30 +0600

    ---- fixxx

---
+++ pages/showcase/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../../layout/Layout";
+
+const Showcase = () => {
+  return (
+    <Layout>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias saepe ea
+      voluptas, quaerat tempore iure obcaecati veniam velit ab. Veniam ipsa
+      error aut molestiae adipisci rerum magni tempora mollitia animi.
+    </Layout>
+  );
+};
+
+export default Showcase;
