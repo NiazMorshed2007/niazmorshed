7b4938812e3f88e979d7591fe178bb959934fdef
NiazMorshed2007 <niazmorshed2007@gmail.com>
2022-08-03 22:22:56 +0600

    ...

--- components/Showcase.jsx
+++ components/Showcase.jsx
@@ -20,11 +20,27 @@ const ShowCase = () => {
             className="project cursor-pointer hover:shadow-md shadow-sky-300 transition-all bg-white/20 backdrop-blur-sm hover:border-slate-400/70 rounded-lg overflow-hidden border border-slate-300"
           >
             <div className="content p-3">
-              <div className="flex items-center justify-between py-1 pb-2 border-b border-slate-300">
+              <div className="py-1 pb-2 border-b border-slate-300">
                 <h1 className="text-lg">{project.name}</h1>
-                <div className="live-preview text-xs">Preview</div>
               </div>
-              <p className="text-sm text-slate-900 mt-2">{project.des}</p>
+              <div className="mt-4">
+                <img className="rounded border" src={project.photo} alt="" />
+                <div className="mt-2">
+                  <span className="text-sm border-b border-dashed border-slate-400 text-orange-800">
+                    Short Description:
+                  </span>
+                  <p className="text-sm text-slate-900 mt-1">
+                    {" " + project.des}
+                  </p>
+                  <p className="my-4 text-sm">
+                    <span className=" text-blue-500">Status: </span>{" "}
+                    <span>Featured</span>
+                    <span className="ml-4 bg-black text-xs text-white p-2 px-3 rounded">
+                      Github repo
+                    </span>
+                  </p>
+                </div>
+              </div>
             </div>
           </motion.div>
         ))}
