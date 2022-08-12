bb68ddad345ee977cda33a367cd7cb3aad98d918
NiazMorshed2007 <niazmorshed2007@gmail.com>
2022-08-12 18:49:00 +0600

    update

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,32 +1,7 @@
 import ExperienceCard from "./ExperienceCard";
+import exps from "../static/experience.json";
 
 const Experience = () => {
-  const exps = [
-    {
-      name: "Uqidev",
-      img: "/uqidev.png",
-      role: "Front End Developer",
-      type: "Full Time",
-      des: "Working as a React Front End Developer. Developing dynamic and complex user interfaces. Have experience of working with E-commerce website. Connected with senior developers. Learned and researched new tech sacks.",
-      lifetime: "Jun 2021 - current",
-    },
-    {
-      name: "Woiqo",
-      img: "/woiqo.png",
-      role: "Front End Developer",
-      type: "Full Time",
-      des: "Worked as a Frontend Web Developer. Worked with React.js and developed simple dynamic/static web apps. Implemented many templates with React and HTML.",
-      lifetime: "Jan 2020 - Apr 2021",
-    },
-    {
-      name: "Maks Inc",
-      img: "/assets/maks.png",
-      role: "Front End Developer",
-      type: "Contract",
-      des: "First project-based job in my dev career. Developed dynamic and complex HR Management Software for Maks Inc. It helps them to manage their projects, employees, clients and more",
-      lifetime: "Jul 2022 - Aug 2022",
-    },
-  ];
   return (
     <section id="experience" className="py-28 relative px-[7%]">
       {/* <div className="absolute bottom-44 left-64 hidden h-[150px] w-[900px] -rotate-45 rounded-3xl bg-gradient-to-r from-violet-600 to-indigo-800 opacity-80 blur-3xl filter dark:block lg:bottom-24 lg:-left-20 lg:h-28 lg:w-[250px] lg:-rotate-12 lg:opacity-20 xl:h-40 xl:w-[200px]"></div> */}
--- components/ExperienceCard.jsx
+++ components/ExperienceCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ExperienceCard = ({ name, img, des, role, type, lifetime }) => {
+const ExperienceCard = ({ title, img, reps, role, type, duration }) => {
   return (
     <div className="uqidev p-3 py-7 rounded-xl border-gray-300/70 border">
       <div className="flex items-center justify-between">
@@ -14,17 +14,21 @@ const ExperienceCard = ({ name, img, des, role, type, lifetime }) => {
           />
           <div>
             <div className="flex md:flex-row flex-col md:items-center gap-1 md:gap-3">
-              <h1 className="text-sm font-semibold">{name}</h1>
+              <h1 className="text-sm font-semibold">{title}</h1>
               <div className="flex md:mb-0 mb-1 items-center gap-3">
                 <span className="text-[10px]">{"(" + type + ")"}</span>
-                <span className="text-[10px]">{"[" + lifetime + "]"}</span>
+                <span className="text-[10px]">{"[" + duration + "]"}</span>
               </div>
             </div>
             <p className=" text-xs">{role}</p>
           </div>
         </div>
       </div>
-      <div className="mt-4 text-[12.5px]  ">{des}</div>
+      <div className="mt-4 text-[12.5px]">
+        {reps.map((rep, i) => (
+            <li key={i}>{rep}</li>
+        ))}
+      </div>
     </div>
   );
 };
