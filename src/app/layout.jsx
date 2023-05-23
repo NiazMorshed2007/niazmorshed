bc452dd9bc10facaea52fa0818b2849d9f614250
NiazMorshed2007 <niazmorshed2007@gmail.com>
2023-05-23 10:16:28 +0600

    update blog link

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,24 +1,29 @@
-import { Inter } from 'next/font/google'
-import Link from 'next/link'
-import { BsGithub, BsLinkedin, BsTwitter } from 'react-icons/bs'
-import Header from './components/Header'
-import './globals.css'
+import { Inter } from "next/font/google";
+import Link from "next/link";
+import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
+import { SiHashnode } from "react-icons/si";
+import Header from "./components/Header";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Niaz Morshed',
-  description: 'Niaz is a frontend developer who has expertise in React, Next, JavaScript and TypeScript. An open source contributor and loves to build things.',
-}
+  title: "Niaz Morshed",
+  description:
+    "Niaz is a frontend developer who has expertise in React, Next, JavaScript and TypeScript. An open source contributor and loves to build things.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      </head>
+      <head></head>
       <body className={inter.className}>
-        <img className="absolute top-0 left-0" style={{
-        }} src="https://app.nhost.io/assets/line-grid.svg" alt="" />
+        <img
+          className="absolute top-0 left-0"
+          style={{}}
+          src="https://app.nhost.io/assets/line-grid.svg"
+          alt=""
+        />
         <div className="fixed z-40 hidden md:flex flex-col items-center gap-7 bottom-12 text-lg text-primary/50 right-16">
           <Link href={"https://twitter.com/niazmorshed_"} target="_blank">
             <BsTwitter />
@@ -29,11 +34,14 @@ export default function RootLayout({ children }) {
           <Link href={"https://linkedin.com/niazmorsheddev"} target="_blank">
             <BsLinkedin />
           </Link>
+          <Link href={"https://niazmorshed.hashnode.dev/"} target="_blank">
+            <SiHashnode />
+          </Link>
           <div className="line w-[1px] h-[50px] bg-primary/50"></div>
         </div>
         <Header />
         {children}
       </body>
     </html>
-  )
+  );
 }
